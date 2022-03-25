f96fd1e3f706fc44f1e1b0dc8a62d6b6758f5113
leeyj-hy <dldydwo1230@hanayng.ac.kr>
2022-03-26 00:18:42 +0900

    heechance

--- src/fiducial_ar_node.cpp
+++ src/fiducial_ar_node.cpp
@@ -28,32 +28,33 @@ private:
     int rot_z;
     int rot_w;
 
-    float x0;
-    float y0;
-    float x1;
-    float y1;
-    float x2;
-    float y2;
-    float x3;
-    float y3;
+    double x0;
+    double y0;
+    double x1;
+    double y1;
+    double x2;
+    double y2;
+    double x3;
+    double y3;
 
-    Mat image;
+    Mat image = Mat(480, 640, CV_8UC3);
 
 public:
-    void sub_node()
+    fiducial_ar_sub()
     {
-        sub1 = n.subscribe("usb_cam/image_raw", 2, &fiducial_ar_sub::imgCallback, this);
+        sub1 = n.subscribe("/usb_cam/image_raw", 2, &fiducial_ar_sub::imgCallback, this);
 
-        sub2 = n.subscribe("fiducial_vertices", 2, &fiducial_ar_sub::mrkrCallback, this);
+        sub2 = n.subscribe("/fiducial_vertices", 2, &fiducial_ar_sub::mrkrCallback, this);
     }
 
     void imgCallback(const sensor_msgs::Image::ConstPtr& msg)
     {
+        ROS_INFO("sub1 start");
         ROS_INFO("Received image with size : %i x %i ", msg -> width, msg -> height);
 
         try
         {
-            cv_bridge::toCvShare(msg, "bgr8") -> image;
+            image = cv_bridge::toCvShare(msg, "bgr8") -> image;
             waitKey(3);
         }
         catch(const cv_bridge::Exception& e)
@@ -64,17 +65,35 @@ public:
 
     void mrkrCallback(const fiducial_msgs::FiducialArray::ConstPtr& msg)
     {
-        this.x0 = msg.vertices.x0;
+        ROS_INFO("sub2 start");
+        for(int i=0; i<msg -> fiducials.size() ; i++)
+        {
+        this -> x0 = msg -> fiducials[i].x0;
+        this -> y0 = msg -> fiducials[i].y0;
+        this -> x1 = msg -> fiducials[i].x1;
+        this -> y1 = msg -> fiducials[i].y1;
+        this -> x2 = msg -> fiducials[i].x2;
+        this -> y2 = msg -> fiducials[i].y2;
+        this -> x3 = msg -> fiducials[i].x3;
+        this -> y3 = msg -> fiducials[i].y3;
+        }
+        ROS_INFO("sub2 end");
 
-
-
+        img_disp();
 
     }
 
-    void img_disp()
+    int img_disp()
     {
-        cv::line(image, Point(x0, y0), Point(x1, y1),Scalar::all(255), 1, 8, 0);
-
+        cv::line(image, Point(this->x0, this->y0), Point(this->x1, this->y1),Scalar(0,0,255), 1, 8, 0);
+        cv::line(image, Point(this->x1, this->y1), Point(this->x2, this->y2),Scalar(0,0,255), 1, 8, 0);
+        cv::line(image, Point(this->x2, this->y2), Point(this->x3, this->y3),Scalar(0,0,255), 1, 8, 0);
+        cv::line(image, Point(this->x3, this->y3), Point(this->x0, this->y0),Scalar(0,0,255), 1, 8, 0);
+        
+        
+        
+        imshow("img", image);
+        waitKey(3);
     }
 
 
@@ -82,10 +101,10 @@ public:
 
 
 
-void main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "fiducial_ar");
-    
+    ros::init(argc, argv, "fiducial_ar_node");
+    //ROS_INFO("ar_node start!");
     fiducial_ar_sub FAS_obj;
 
     ros::spin();
