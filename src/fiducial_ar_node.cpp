9d8b601729164c27dde6c2391d748644766fd950
leeyj-hy <dldydwo1230@hanyang.ac.kr>
2022-03-25 21:39:00 +0900

    class added

--- src/fiducial_ar_node.cpp
+++ src/fiducial_ar_node.cpp
@@ -28,30 +28,69 @@ private:
     int rot_z;
     int rot_w;
 
+    float x0;
+    float y0;
+    float x1;
+    float y1;
+    float x2;
+    float y2;
+    float x3;
+    float y3;
+
+    Mat image;
+
 public:
     void sub_node()
     {
         sub1 = n.subscribe("usb_cam/image_raw", 2, &fiducial_ar_sub::imgCallback, this);
+
+        sub2 = n.subscribe("fiducial_vertices", 2, &fiducial_ar_sub::mrkrCallback, this);
     }
 
-    void imgCallback()
+    void imgCallback(const sensor_msgs::Image::ConstPtr& msg)
     {
+        ROS_INFO("Received image with size : %i x %i ", msg -> width, msg -> height);
+
+        try
+        {
+            cv_bridge::toCvShare(msg, "bgr8") -> image;
+            waitKey(3);
+        }
+        catch(const cv_bridge::Exception& e)
+        {
+            ROS_ERROR("Could not convert from '&s' to 'bgr8 .", msg -> encoding.c_str());    
+        }
+    }
+
+    void mrkrCallback(const fiducial_msgs::FiducialArray::ConstPtr& msg)
+    {
+        this.x0 = msg.vertices.x0;
+
+
+
 
     }
+
+    void img_disp()
+    {
+        cv::line(image, Point(x0, y0), Point(x1, y1),Scalar::all(255), 1, 8, 0);
+
+    }
+
+
 };
 
-fiducial_ar_sub::fiducial_ar_sub(/* args */)
+
+
+void main(int argc, char **argv)
 {
+    ros::init(argc, argv, "fiducial_ar");
+    
+    fiducial_ar_sub FAS_obj;
+
+    ros::spin();
+    return 0;
 }
-
-fiducial_ar_sub::~fiducial_ar_sub()
-{
-}
-
-
-
-
-
 
 
 
